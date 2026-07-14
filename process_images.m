5b64ae34a26ba3621c2e6c9ba6d33b50f6c6a8c8
dubi du <100496683@alumnos.uc3m.es>
2026-07-14 12:22:46 +0200

    randomizado

--- process_images.m
+++ process_images.m
@@ -1,19 +1,28 @@
-function T = process_images()
+function [T,chosen_people] = process_images()
 % Takes the image dataset and creates an organized tensor
-resize_thing = [48,64]; % decimal -> resize factor, array -> resolution
-size_people = 50;
 
-chosen_people = 1:50; % in the future it will be random
+resize_thing = [48,64]; % decimal -> resize factor, array -> resolution
+
+% Choose the people we will be analyzing
+size_people = 10;
+chosen_people = randperm(200,size_people);
 
 size_emotion = 13;
-T = ones([resize_thing(1)*resize_thing(2),size_emotion,size_people]);
+test_size = 1;
+T = ones([resize_thing(1)*resize_thing(2),size_emotion-test_size,size_people]);
 
-for person = chosen_people
-    chosen_emotions = 1:size_emotion; % could be different for each person 
-    for emotion = chosen_emotions
-        img_name = "datasets/FEI_face_database/combined/" + num2str(person) + "-" + num2str(emotion,'%02d') + ".jpg";
+for i = 1:size_people
+    
+    % Choose a subset of emotions for training and leave some for testing.
+    % They are unsorted
+    chosen_emotions = randperm(size_emotion,size_emotion-test_size);
+    
+    %chosen_emotions = 1:size_emotion;
+
+    for j = 1:size_emotion-test_size
+        img_name = "datasets/FEI_face_database/combined/" + num2str(chosen_people(i)) + "-" + num2str(chosen_emotions(j),'%02d') + ".jpg";
         img = imread(img_name);
         mod_img = rgb2gray(imresize(img,resize_thing)); % to gray scale and to smaller size
-        T(:,emotion,person) = mod_img(:);
+        T(:,j,i) = mod_img(:);
     end
 end
