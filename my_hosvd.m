f961ead809a2fe097eda0e4fd8c92ec48d088a8c
dubi du <100496683@alumnos.uc3m.es>
2026-07-15 14:52:03 +0200

    nos va a dar problemas la tonteria del svd

--- my_hosvd.m
+++ my_hosvd.m
@@ -1,6 +1,6 @@
 function [U1,U2,U3,S,s1,s2,s3]=my_hosvd(A)
 % Compute the HOSVD of a 3-way tensor A
-[U1,s1,~]=svd(unfold3(A,1),"econ");
-[U2,s2,~]=svd(unfold3(A,2),"econ");
-[U3,s3,~]=svd(unfold3(A,3),"econ");
+[U1,s1,~]=svd(unfold3(A,1));
+[U2,s2,~]=svd(unfold3(A,2));
+[U3,s3,~]=svd(unfold3(A,3));
 S=my_tmul(my_tmul(my_tmul(A,U1,1),U2,2),U3,3);
