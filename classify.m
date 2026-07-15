5f333e4acae8f806932106b2c029dbaafdb5b98b
dubi du <100496683@alumnos.uc3m.es>
2026-07-15 14:42:05 +0200

    casi casi tenemos clasificacion hecha

---
+++ classify.m
@@ -0,0 +1,24 @@
+function [best_p,best_e] = classify(T,z)
+% Given a tensor of people with emotions and a z column of that tensor,
+% finds who it is (from the given options)
+[U1,U2,U3,S,s1,s2,s3] = my_hosvd(T);
+size(T)
+B = my_tmul(S,U2,2);
+z_hat = transpose(U1)*z;
+
+best_diff = inf;
+best_p = 0;
+best_e = 0;
+for e = 1:size(T,2)
+    [Q,R] = qr(reshape(B(:,e,:),[size(T,1),size(T,3)]));
+    R_e = R(1:size(T,3),:);
+    Q_e = Q(:,1:size(T,3));
+    x = R_e\(transpose(Q_e)*z_hat);
+    for p = 1:people
+        h_p = transpose(U3(p,:));
+        if norm(x-h_p) < best_diff
+            best_e = e;
+            best_p = p;
+        end
+    end
+end
--- process_images.m
+++ process_images.m
@@ -1,28 +1,29 @@
-function [T,chosen_people] = process_images()
-% Takes the image dataset and creates an organized tensor
-
-resize_thing = [48,64]; % decimal -> resize factor, array -> resolution
+function [T,chosen_people] = process_images(resize_param, size_people, size_emotion,randomize)
+% Takes the image dataset and creates an organized tensor. First coordinate is the image mode, second is the emotion mode and third is people mode
+arguments
+    resize_param = [48,64]; % decimal -> resize factor, array -> resolution
+    size_people = 10;
+    size_emotion = 13; % all but one are used for training
+    randomize = false;
+end
 
 % Choose the people we will be analyzing
-size_people = 10;
 chosen_people = randperm(200,size_people);
-
-size_emotion = 13;
-test_size = 1;
-T = ones([resize_thing(1)*resize_thing(2),size_emotion-test_size,size_people]);
+T = ones([resize_param(1)*resize_param(2),size_emotion,size_people]);
 
 for i = 1:size_people
-    
-    % Choose a subset of emotions for training and leave some for testing.
-    % They are unsorted
-    chosen_emotions = randperm(size_emotion,size_emotion-test_size);
-    
-    %chosen_emotions = 1:size_emotion;
+    if randomize
+        % Choose a subset of emotions for training and leave some for testing.
+        % They are unsorted
+        chosen_emotions = randperm(size_emotion,size_emotion);
+    else
+        chosen_emotions = 1:size_emotion;
+    end
 
-    for j = 1:size_emotion-test_size
+    for j = size_emotion
         img_name = "datasets/FEI_face_database/combined/" + num2str(chosen_people(i)) + "-" + num2str(chosen_emotions(j),'%02d') + ".jpg";
         img = imread(img_name);
-        mod_img = rgb2gray(imresize(img,resize_thing)); % to gray scale and to smaller size
+        mod_img = rgb2gray(imresize(img,resize_param)); % to gray scale and to smaller size
         T(:,j,i) = mod_img(:);
     end
 end
