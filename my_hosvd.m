e44a09dfd0c23b06ad0968ce3ae6bb53b8620f8e
dubi du <100496683@alumnos.uc3m.es>
2026-07-16 13:15:25 +0200

    todomaltodomaltodomaltodomal

--- my_hosvd.m
+++ my_hosvd.m
@@ -3,4 +3,4 @@ function [U1,U2,U3,S,s1,s2,s3]=my_hosvd(A)
 [U1,s1,~]=svd(unfold3(A,1));
 [U2,s2,~]=svd(unfold3(A,2));
 [U3,s3,~]=svd(unfold3(A,3));
-S=my_tmul(my_tmul(my_tmul(A,U1,1),U2,2),U3,3);
+S=my_tmul(my_tmul(my_tmul(A,transpose(U1),1),transpose(U2),2),transpose(U3),3);
