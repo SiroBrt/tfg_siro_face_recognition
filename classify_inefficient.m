2dc87b50b696453fb8e8802701db2b0c4dd1475f
dubi du <100496683@alumnos.uc3m.es>
2026-07-17 13:12:01 +0200

    funcionando

--- classify_inefficient.m
+++ classify_inefficient.m
@@ -1,20 +1,17 @@
-function [best_p,best_e] = classify(T,z)
+function [best_p,best_e] = classify_inefficient(T,z)
 % Given a tensor of people with emotions and a z column of that tensor,
 % finds who it is (from the given options)
-[U1,U2,U3,S,s1,s2,s3] = my_hosvd(T);
-B = my_tmul(S,U2,2);
-z_hat = transpose(U1)*z;
+[U1,U2,U3,S,~,~,~] = my_hosvd(T);
+C = my_tmul(my_tmul(S,U1,1),U2,2);
 
 best_diff = inf;
 best_p = 0;
 best_e = 0;
 H = transpose(U3);
 for e = 1:size(T,2)
-    [Q,R] = qr(reshape(B(:,e,:),[size(T,1),size(T,3)]));
-    R_e = R(1:size(T,3),:);
-    Q_e = Q(:,1:size(T,3));
-    x = R_e\(transpose(Q_e)*z_hat);
-    
+    C_e = reshape(C(:,e,:),[size(T,1),size(T,3)]);
+   
+    x = lsqr(C_e,z);
     for p = 1:size(T,3)
         h_p = H(:,p);
         if norm(x-h_p) < best_diff
