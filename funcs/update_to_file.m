4e601384b1b58fb934d64c25bf69c0c335674bd9
dubi du <100496683@alumnos.uc3m.es>
2026-07-21 19:27:57 +0200

    bendito sea quien mató al inventor de sql

--- funcs/update_to_file.m
+++ funcs/update_to_file.m
@@ -1,11 +1,27 @@
 function update_to_file(file_name,tabla)
-% put keys as string, anything else will get combined (added) between the file and the new table
+% put keys as string at the beginning, anything else will get combined (added) between the file and the new table
 if isfile(file_name)
     prev = readtable('data/resolution_vs_people_accuracy.csv');
     prev.Properties.VariableTypes = tabla.Properties.VariableTypes;
-
-    writetable(union(prev,tabla),file_name);
     
+    % identify columns
+    strings = tabla.Properties.VariableTypes == "string";
+    keys = tabla.Properties.VariableNames(strings);
+    values = tabla.Properties.VariableNames(~strings);
+
+    % join
+    combined = outerjoin(tabla,prev,Keys=keys,MergeKeys=true);
+    
+    % combine things
+    for i = 1:length(values)
+        v =  transpose(sum(transpose(table2array([combined(:,length(keys)+i) combined(:,length(keys)+i+length(values))])),'omitnan'));
+        combined = addvars(combined,v,NewVariableNames=values(i));
+    end
+    
+    %remove unnecesary columns
+    combined(:,length(keys)+1:length(keys)+2*length(values)) = [];
+
+    writetable(combined,file_name)
 else
     writetable(tabla,file_name)
 end
